dc1b1b86b76ef8357dd619381025f1fb5e8b4fad
vellnWan <135400316+vellnWan@users.noreply.github.com>
2024-04-21 14:54:56 +0800

    Update poi_segmentation.py

--- poi_segmentation.py
+++ poi_segmentation.py
@@ -9,17 +9,17 @@ def find_encoding(fname):
     charenc = result['encoding']
     return charenc
 # 指定源文件所在的目录
-source_directory = 'E:\\poi\\'
+source_directory = '输入路径'
 # 指定保存新生成文件的目录
-destination_directory = 'E:\\POI1\\'
+destination_directory = '输出路径'
 # 循环处理源目录下的所有csv文件
 for filepath in glob.glob(source_directory + '*.csv'):
     # 获取文件的编码格式
     my_encoding = find_encoding(filepath)
     # 读取csv文件到df中，注意编码格式
     df = pd.read_csv(filepath, encoding=my_encoding)
-    # 使用str.split方法，将'location'列根据逗号分割成两列'location_x'和 'location_y'
-    df[['location_x', 'location_y']] = df['location'].str.split('，', expand=True).astype(float)
+    # 使用str.split方法，将'location'列根据逗号分割成两列'lat'和 'lon'
+    df[['lat', 'lon']] = df['location'].str.split('，', expand=True).astype(float)
     # 构造新生成文件的全路径名
     new_filepath = os.path.join(destination_directory,os.path.basename(filepath).replace('.csv', '_new.csv'))
     # 将处理后的DataFrame保存到新文件里，并使用源文件相同的编码
